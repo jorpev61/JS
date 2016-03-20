b5e6d3bbd795bf6724f7e2702d0583e6ec243b9d
Jorge Perero <jorpev85@hotmail.com>
2016-03-20 17:27:09 -0500

    Práctica del cuenta cuentos

--- scripts.js
+++ scripts.js
@@ -1,5 +1,22 @@
-console.log("inicio de programa");
+/*console.log("inicio de programa");
 alert("hola mundo");
 document.write("<h1>Hola JavaScript</h1>");
 alert("Gracias por visitar");
-console.log("Fin de programa");
+console.log("Fin de programa");*/
+
+/*var equipo=prompt("Cual es tu equipo de futbol favorito");
+console.log("El equipo es:"+equipo);*/
+
+/*var clave=prompt("Ingrese su clave!");
+console.log("La longitud de la clave es:" + clave.length);*/
+/*document.write("<h1>Hola JavaScript</h1>" + clave);*/
+/*console.log("Su clave es:" + clave.toUpperCase());
+console.log("El valor original de la clave es:" + clave);*/
+
+var adjetivo=prompt("Adejtivo");
+var sustantivo=prompt("Sustantivo");
+var verbo=prompt("Verbo");
+console.log("La frase:>" + adjetivo + sustantivo + verbo);
+document.write("<h1><p>Caminaba en el malecón cuando el " + adjetivo + " " + sustantivo + " " + verbo + " por la playa</p></h1>");
+document.write("<h1><p>Me paré en la orilla cuando " + adjetivo + " " + sustantivo + " " + verbo + " dentro del mar</p></h1>");
+document.write("<h1><p>El " + adjetivo + " " + sustantivo + " " + verbo + " cerca de los yates</p></h1>");
