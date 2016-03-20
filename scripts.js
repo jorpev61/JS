7db2c9bee406b01279e9629127078f8d7d4ff622
Armando Alejandro <aalejandro1708@gmail.com>
2016-03-20 15:30:00 -0500

    Correcciones en el codigo del script, archivo funcional

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-console.log("inicio de programa
+console.log("inicio de programa");
 alert("hola mundo");
-document.write("<h1>Hola JavaScript");
+document.write("<h1>Hola JavaScript</h1>");
 alert("Gracias por visitar");
 console.log("Fin de programa");
