556cc19ee79ffbeb318a6402901b704cedcafd2f
Jorge Perero <jorpev85@hotmail.com>
2016-03-27 09:29:08 -0500

    Calculadora del tiempo

--- scripts.js
+++ scripts.js
@@ -13,10 +13,28 @@ console.log("La longitud de la clave es:" + clave.length);*/
 /*console.log("Su clave es:" + clave.toUpperCase());
 console.log("El valor original de la clave es:" + clave);*/
 
-var adjetivo=prompt("Adejtivo");
+/*var adjetivo=prompt("Adejtivo");
 var sustantivo=prompt("Sustantivo");
 var verbo=prompt("Verbo");
 console.log("La frase:>" + adjetivo + sustantivo + verbo);
 document.write("<h1><p>Caminaba en el malecón cuando el " + adjetivo + " " + sustantivo + " " + verbo + " por la playa</p></h1>");
 document.write("<h1><p>Me paré en la orilla cuando " + adjetivo + " " + sustantivo + " " + verbo + " dentro del mar</p></h1>");
-document.write("<h1><p>El " + adjetivo + " " + sustantivo + " " + verbo + " cerca de los yates</p></h1>");
+document.write("<h1><p>El " + adjetivo + " " + sustantivo + " " + verbo + " cerca de los yates</p></h1>");*/
+
+/*var minutosPorHora = 60;
+var horasPorDia = 24;
+var segundosPorDia = segundosPorMinuto*minutosPorHora*horasPorDia;
+var edad=prompt("Ingrese su edad");
+var diasVida = edad*segundosPorDia;
+
+document.write("<h1>Calculadora del Tiempo</h1><p>Hay "+segundosPorDia +" en un día</p>");
+document.write("<h1>Dias vividos</h1><p>Tienes "+diasVida +" días de vida</p>");*/
+
+console.log("inicio de programa");
+
+var peliculasVistas=prompt("Peliculas vistas por mes");
+var seriesVistas=prompt("Series vistas por mes");
+var tiempoPantalla=parseInt(peliculasVistas) + parseInt(seriesVistas);
+alert('Wow! Has visto ' +tiempoPantalla+ " veces minimo la pantalla");
+
+console.log("Fin de programa");
